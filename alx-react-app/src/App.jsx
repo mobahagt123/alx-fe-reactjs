9c523ee5cedd55ace709dd952f208cf9924a04c9
mohamedbahgat <mohamedbahgat950@gmail.com>
2024-11-02 21:22:37 +0300

    userprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,13 +6,12 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
 
 function App() {
   return (
     <>
-      <MainContent />
-      <Header />
-      <Footer />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
     </>
   )
 }
