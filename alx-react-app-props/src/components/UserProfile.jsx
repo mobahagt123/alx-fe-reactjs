17935e08697dac2c1b32730072a1fe2ad23d7bc3
mohamedbahgat <mohamedbahgat950@gmail.com>
2024-11-09 16:34:43 +0300

    UserData

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,7 +1,15 @@
 import UserInfo from "./ProfilePage/UserInfo/UserInfo";
+import UserContext from "./UserContext";
+import { useContext } from "react";
 
 function ProfilePage() {
-  return <UserInfo />;
+  const userData = useContext(UserContext)
+  return (
+    <div>
+      <p>Name: {userData.name}</p>
+      <p>Email: {userData.email}</p>
+    </div>
+  );
 }
 
 export default ProfilePage;
