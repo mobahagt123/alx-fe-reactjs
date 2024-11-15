239a46f5779793dd59d8197ea180523e013fdee3
mohamedbahgat <mohamedbahgat950@gmail.com>
2024-11-15 20:22:38 +0300

    useRecipeStore

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
-import useRecipeStore from '../store/useRecipeStore'
+import useRecipeStore from './recipeStore'
 
 function AddRecipeForm() {
     const [title ,setTitle] = useState('')
