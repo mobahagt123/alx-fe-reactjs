dee1a970404b73f38f5031e24f112283904e2a77
mohamedbahgat <mohamedbahgat950@gmail.com>
2024-11-15 20:01:51 +0300

    create components and add store

---
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { useState } from 'react'
+import useRecipeStore from '../store/useRecipeStore'
+
+function AddRecipeForm() {
+    const [title ,setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const addRecipe = useRecipeStore(state => state.addRecipe)
+
+    const handleSubmit = (event)=> {
+        event.preventDefault()
+        addRecipe({
+            id: Date.now(),
+            title: title,
+            description: description
+        });
+        setTitle('')
+        setDescription('')
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type='text' 
+                   value={title} 
+                   onChange={(e)=> setTitle(e.target.value)} 
+                   placeholder='Recipe Title'
+            />
+            <textarea
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                placeholder='Recipe Description'
+            />
+            <button type='submit'>Add Recipe</button>
+        </form>
+    )
+    }
+
+export default AddRecipeForm;
