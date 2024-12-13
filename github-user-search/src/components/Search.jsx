0b5651c94084787a397792898e7aa8beb038a9fd
mohamedbahgat <mohamedbahgat950@gmail.com>
2024-12-14 00:50:15 +0300

    implement search component and fetch service

---
+++ github-user-search/src/components/Search.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import fetchUserData from '../services/githubService'
+
+
+function Search() {
+
+    const [username, setUsername] = useState('')
+    const [data , setData] = useState('')
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(true)
+
+    
+    const getData = async()=> {
+        try {
+            await fetchUserData(username)
+            .then(response => setData(response.data))
+            setLoading(true)
+        } catch (error) {
+            setError(error)
+
+        }
+    }
+
+    const handleSubmit = (e)=> {
+        e.preventDefault()
+        getData()
+    }
+ 
+        
+  return (
+    <div>
+        <form onSubmit={handleSubmit} >
+            <input type='text'
+                   placeholder='Enter github usename'
+                   onChange={(e)=>setUsername(()=> e.target.value)} />
+
+            <button type='submit'>Search</button>
+        </form>
+        <div> 
+            <div>
+                {error ? <div>Looks like we cant find the user</div> : <div>
+                {loading ? <div>
+                                <img src={data.avatar_url} />
+                                <span>{data.name}</span>
+                                <span>{data.bio}</span>
+                           </div> 
+                : <div>Loading...</div>}
+            </div>}
+            </div>
+            
+            
+
+
+          
+        </div>
+    </div>
+  )
+}
+
+export default Search
---
+++ github-user-search/src/services/githubService.js
@@ -0,0 +1,20 @@
+
+import axios from 'axios'
+
+
+
+
+const fetchUserData = (username)=> {
+
+    return (
+         axios.get(`https://api.github.com/users/${username}` , {
+        headers: {
+            Authorization: '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',
+        }
+     })   
+    )
+    
+}
+
+export default fetchUserData;
+
