d13bb4960a8934ea8e8aa772e965e97c71da8068
mohamedbahgat <mohamedbahgat950@gmail.com>
2024-12-14 01:03:35 +0300

    implement search component and fetch service

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -9,7 +9,7 @@ const fetchUserData = (username)=> {
     return (
          axios.get(`https://api.github.com/users/${username}` , {
         headers: {
-            Authorization: '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',
+            Authorization:  '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' ,
         }
      })   
     )
